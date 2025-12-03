be35c8caf6933b8dff2fe91c92562285cc49fa65
nyaago <nyaago69@gmail.com>
2025-12-04 01:10:34 +0900

    Create project

---
+++ Note/NoteTests/NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTests.swift
+//  NoteTests
+//
+//  Created by nyaago on 2025/12/04.
+//
+
+import Testing
+@testable import Note
+
+struct NoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
