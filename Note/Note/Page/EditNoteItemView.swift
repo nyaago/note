526c82e2d83428b69f519e08c62f62466d78e9e6
nyaago <nyaago69@gmail.com>
2026-01-06 23:53:22 +0900

    Change data if it has been a while since the last change

--- Note/Note/Page/EditNoteItemView.swift
+++ Note/Note/Page/EditNoteItemView.swift
@@ -12,12 +12,15 @@ struct EditNoteItemView: View {
     @State var content: String
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
+    @State private var savedAt: Date?
 
     init(note: Note) {
         self.note = note
         self.content = note.content
+        self.savedAt = Date()
     }
     
+    // preview 用
     init(content: String) {
         self.content = content
     }
@@ -32,6 +35,9 @@ struct EditNoteItemView: View {
             .modifier(TextEditorModifier())
             .onAppear() {
             }
+            .onChange(of: content) {
+                changeNoteContentIfNeed()
+            }
             .navigationBarBackButtonHidden(true)
             .toolbar(content: {
                 doneToolbarContent
@@ -42,14 +48,33 @@ struct EditNoteItemView: View {
         let text: String = "Done"
         return ToolbarItem(placement: .navigationBarLeading) {
             Button(text, action: {
-                guard let note = self.note else {
-                    dismiss()
-                    return
-                }
-                note.content = content
-                dismiss()
+                changeNoteContentAndDismiss()
             })
         }
+    }
+    
+    private func changeNoteContent() {
+        guard let note = self.note else {
+            return
+        }
+        note.content = content
+        note.timestamp = Date()
+        self.savedAt = Date()
+    }
+    
+    private func changeNoteContentIfNeed() {
+        let timeIntervalToAdd: TimeInterval = 30
+        guard let savedAt = self.savedAt else {
+            return
+        }
+        if Date().timeIntervalSince(savedAt) > timeIntervalToAdd {
+            changeNoteContent()
+        }
+    }
+    
+    private func changeNoteContentAndDismiss() {
+        changeNoteContent()
+        dismiss()
     }
 }
 
